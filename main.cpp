dbfc0ce9633b6f714cb1af27f7e7b6abfba3a781
Aboubakr Krid <akrid@c3r10p9.1337.ma>
2025-08-16 11:17:22 +0100

    some trush .

--- main.cpp
+++ main.cpp
@@ -1,42 +1,18 @@
-#include <iostream>
-#include <vector>
+#include "main.hpp"
 
-class Client
+
+void    data_init()
 {
-private:
-    int             _fd;
-    std::string     _IPadd;
-public:
-    Client():_fd(-1){};
-    ~Client(){};
-    void    setFD(int fd){_fd = fd;};
-    int     getFD() {return _fd;};
-    void    setAdd(std::string add){_IPadd = add;};
-};
+    
+}
 
-class Server
+int main(int ac, char** av)
 {
-private:
-    int                         port;
-    int                         serSocketFd;
-    static bool                 signal;
-    std::vector<Client>         clients;
-    std::vector<struct pollfd>  fds;
-public:
-    Server():serSocketFd(-1){};
-    ~Server(){};
-    void    serverInit();
-    void    serSocket();
-    void    acceptNewClient();
-    void    receiveNewData(int fd);
-    static void     signalHandler(int signum);
-    void            closeFds();
-    void            clearClients(int fds);
-};
-
-
-int main()
-{
-
+    if (ac != 3)
+    {
+        std::cout << "usage : ./ircserv IPaddr port" << std::endl;
+        return 1;
+    }
+    data_init();
     return 0;
 }
